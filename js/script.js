6a0426f371b96d4fbde5bb9cc9260d838de7e6b0
Trotteriniku <stefanozidda3@gmail.com>
2023-10-23 23:38:47 +0200

    added rowEl+resultsEl

--- js/script.js
+++ js/script.js
@@ -6,3 +6,8 @@ const numberQua = 5;
 const numbertoGuess = [];
 
 const timer = 30000;
+
+const rowEl = document.querySelector('.row');
+rowEl.innerHTML=''
+const resultsEl = document.getElementById('results');
+results.innerHTML = '';
